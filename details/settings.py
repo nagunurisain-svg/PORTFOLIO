ee57fd0c3a4f9dffab538ece4c44006035fff6be
Nagunuri <arunsaiarunsai320@Gmail.com>
2026-09-13 10:43:56 +0530

    Add StudentHub APK

--- details/settings.py
+++ details/settings.py
@@ -114,18 +114,26 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static Files
+# =========================================================
+# STATIC FILES
+# =========================================================
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'student' / 'static',
+    BASE_DIR / "student" / "static",
 ]
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 
 # MEDIA FILES
